e5c739a9f7c9097e1c1d69582ad6123cd293f8bf
Nick <nauynix@gmail.com>
2025-12-25 16:57:26 +0000

    add quickfix commands

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -11,8 +11,32 @@ return {
     -- Mappings can be configured through AstroCore as well.
     mappings = {
       n = {
+        -- Quickfix
+        ["<A-j>"] = { "<cmd>cnext<cr>", desc = "Next quickfix item" },
+        ["<A-k>"] = { "<cmd>cprev<cr>", desc = "Previous quickfix item" }
+        ["<A-q>"] = {
+          function()
+            local qf_exists = false
+            for _, win in pairs(vim.fn.getwininfo()) do
+              if win["quickfix"] == 1 then qf_exists = true end
+            end
+            if qf_exists then
+              vim.cmd("cclose")
+            else
+              -- Only open if there are actually items in the list
+              if not vim.tbl_isempty(vim.fn.getqflist()) then
+                vim.cmd("copen")
+              else
+                print("Quickfix list is empty")
+              end
+            end
+          end,
+          desc = "Toggle Quickfix Window",
+        },
+        -- Leap
         s = { "<Plug>(leap)", desc = "Leap to target" },
         S = { "<Plug>(leap-from-window)", desc = "Leap from window" },
+        -- Search
         ["<Leader>fw"] = {
           function()
             local exclusion_globs = {
