824d31493341563acb4c5fabb7133d2d9dca9f2b
xinyuan-gf <xinyuan-gf@thoughtmachine.net>
2026-03-04 09:35:30 +0000

    switch to osc52 instead of xclip for copy paste, trialing

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -2,3 +2,20 @@
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 require("smart-paste").setup()
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  underline = false,
+  virtual_text = false,
+  update_in_insert = false,
+})
+
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
