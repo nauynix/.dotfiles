46e407abe264e7663622ff8e68dc889f0d76ff38
nauynix <nauynix@gmail.com>
2026-03-13 09:25:08 +0000

    add neovide

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,6 +1,19 @@
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0.03
+  vim.g.neovide_cursor_trail_size = 0.4
+  vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  vim.g.neovide_cursor_vfx_particle_density = 5.0
+  vim.g.neovide_cursor_animate_in_insert_mode = true
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_scroll_animation_length = 0.15
+  vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_hide_mouse_when_typing = true
+end
 require("smart-paste").setup()
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   underline = false,
